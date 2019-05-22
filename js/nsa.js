270742fa2db951222581f36f12db8e67abad2351
vnptmyenglish <50893124+vnptmyenglish@users.noreply.github.com>
2019-05-22 11:42:41 +0700

    new

--- js/nsa.js
+++
@@ -1,4 +0,0 @@
-/*! HelpSmith Web Help System 3.0
-* http://www.helpsmith.com
-* Copyright (c) 2007-2019 Divcom Software */
-$(document).ready(function(){$(window).on("resize",function(){alignDocument()}).trigger("resize")});function alignDocument(){var b=$("#non-scrolling-area");if(!(0>=b.length)){var a=$("#container");if(!(0>=a.length)){var c=a.find("> #content-background");if(!(0>=c.length)){var d=c.find("> #content");if(!(0>=d.length))try{var e=b.outerHeight(!0);a.css({top:b.position().top+e,height:$(window).innerHeight()-e});c.css({height:Math.max(d.innerHeight(),a.height())})}catch(f){}}}}};
--- js/settings.js
+++
@@ -1,8 +0,0 @@
-﻿var whSettings = {
-	highlightSearchWords : true,
-	highlightSearchWordsSingleColor : false,
-	tocOnlyExpandSingleHeading : false,
-	tocAutoExpandOnActivate : false,
-	tocAtimateExpandingCollapsingDuration : 200,
-	indexFile : 'index.htm'
-}
